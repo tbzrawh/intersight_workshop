57cd232fae631f6517f05dd68ee98d1766198859
Michael Maurer <mimaurer@cisco.com>
2020-06-14 15:11:11 +0200

    added

---
+++ code.py
@@ -0,0 +1,8 @@
+import intersight
+from intersight.intersight_api_client import IntersightApiClient
+import json
+
+api_instance = IntersightApiClient(
+    private_key="./SecretKey.txt",
+    api_key_id="EIGENE_KEY_ID_HIER_EINFÜGEN",
+)
