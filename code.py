d3bd2865f0166065173a84f090d0066b7335537e
Michael Maurer <mimaurer@cisco.com>
2020-06-14 15:12:37 +0200

    added

--- code.py
+++ code.py
@@ -1,3 +1,6 @@
+# Aufgabe 1 --> in Intersight
+
+# Aufgabe 2
 import intersight
 from intersight.intersight_api_client import IntersightApiClient
 import json
@@ -6,3 +9,20 @@ api_instance = IntersightApiClient(
     private_key="./SecretKey.txt",
     api_key_id="EIGENE_KEY_ID_HIER_EINFÜGEN",
 )
+
+# Aufgabe 3
+
+
+# Aufgabe 4
+
+
+# Aufgabe 5
+
+
+# Aufgabe 6
+
+
+# Aufgabe 7
+
+
+# Aufgabe 8 --> in Intersight
